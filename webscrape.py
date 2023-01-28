0e85972b0bd73140f84d852923c3e9ccefb22e74
ctgchris <ctgchris@github.com>
2023-01-28 17:47:21 -0500

    add comments

--- webscrape.py
+++ webscrape.py
@@ -2,23 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+#dataset to be used
 url = 'https://www.numbeo.com/food-prices/country_result.jsp?country=United+States'
 page = requests.get(url)
+#grab all the html from website
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Find the table containing the data
+#add all prices to prices array
 prices = soup.find_all('span', class_='first_currency')
-# Extract the rows from the table
 for i in prices:
     print(i.text)
-table = soup.find('table', {'class': 'data_wide_table'})
-
-# Extract the text contents of the first column of each row
-#products = [row.find('td', {'class': 'first'}).text for row in rows]
-
-#print(products)
-
+#retrieve table values from website
 tables = pd.read_html(url)
-arr = (tables[2][0])
-for i in arr:
+#add all food names from first coloumn of table to name array
+name = (tables[2][0])
+for i in name:
     print(i)
