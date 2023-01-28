6aa86df3fdfc872e4a1270d8a2c981fee84dcafb
ctgchris <ctgchris@github.com>
2023-01-28 11:49:46 -0500

    retrieve product and prices

--- webscrape.py
+++ webscrape.py
@@ -1,1 +1,13 @@
-print("Hi")
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.comparegroceryprices.org/search/data/comparison.shtml"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+
+products = soup.find_all("td", {"class": "list"})
+for product in products:
+    name = product.find("a").text
+    walmart_price = product.find("td", {"class": "walmart"}).text
+    print(name, walmart_price)
