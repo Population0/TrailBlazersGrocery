5feb472e9e319eee3c9fc4eef939e9518dfe76da
ctgchris <ctgchris@github.com>
2023-01-28 17:37:25 -0500

    finish webscraper for products and prices

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = 'https://www.numbeo.com/food-prices/country_result.jsp?country=United+States'
 page = requests.get(url)
@@ -10,9 +11,14 @@ prices = soup.find_all('span', class_='first_currency')
 # Extract the rows from the table
 for i in prices:
     print(i.text)
+table = soup.find('table', {'class': 'data_wide_table'})
 
-food = soup.find(id='tbody')
-names = food.find_all('td')
+# Extract the text contents of the first column of each row
+#products = [row.find('td', {'class': 'first'}).text for row in rows]
 
-for i in food:
-    print(i.txt)
+#print(products)
+
+tables = pd.read_html(url)
+arr = (tables[2][0])
+for i in arr:
+    print(i)
