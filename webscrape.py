2edb75a6b48d0d15ac69a6ad0273245b9f2423e5
AlexPham104 <alexander.mt.pham@gmail.com>
2023-01-28 01:52:00 -0500

    empty commit

--- webscrape.py
+++ webscrape.py
@@ -5,14 +5,6 @@ url = 'https://www.numbeo.com/food-prices/country_result.jsp?country=United+Stat
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-# Find the table containing the data
-prices = soup.find_all('span', class_='first_currency')
-# Extract the rows from the table
-for i in prices:
-    print(i.text)
 
-food = soup.find(id='tbody')
-names = food.find_all('td')
 
-for i in food:
-    print(i.txt)
+
