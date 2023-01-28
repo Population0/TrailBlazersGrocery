fc8e43b9a4e92a6021a634cd5bd2f020171cc643
arghom <argho9703@gmail.com>
2023-01-28 17:14:48 -0500

    created template website and created working list

--- react-element/src/App.js
+++ react-element/src/App.js
@@ -1,100 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App(props) {
-  return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
-      <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+import React, { Component, createElement } from "react";
+import { findByLabelText } from '@testing-library/react';
+
+export default class TodoList extends Component {
+  state = {
+    todo: [],
+    inputValue: ""
+  };
+
+  addTodo = () => {
+    this.setState({ todo: [this.state.inputValue, ...this.state.todo] });
+
+    // I want to insert separate paragraph tags (todos from this.state.todo) into the list element here
+  };
+
+  handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      if (this.state.inputValue === "") return;
+
+      this.addTodo();
+      this.state.inputValue = "";
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  render() {
+    const mystyle = {
+      color: "white",
+      backgroundColor: "DodgerBlue",
+      padding: "10px",
+      fontFamily: "Arial",
+      display: "flex",
+      flexDirection: "row"
+    };
+    const mainBox = {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column"
+    };
+    const listStyle = {
+      display: "flex",
+      alignItems: "row"
+    };
+    return (
+      <div style={mainBox}>
+        <h1>
+          LETS START WHOPPING
+        </h1>
+        <div style={mystyle}>
+          <input
+            type="text"
+            className="insertTodo"
+            placeholder="Add a new item!"
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+            value={this.state.inputValue}
+          />
+          <button onClick={this.addTodo}>Add to list</button>
+        </div>
+        <div className="list">
+            {this.state.todo.map((todo) => {
+              return <p>{todo}</p>;
+            })}
+          </div>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading">
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              Eat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
-export default App;
