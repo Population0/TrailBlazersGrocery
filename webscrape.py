1c85ca15d74e9b5b61c2602b6a75c0cdcaa7b7b6
ctgchris <ctgchris@github.com>
2023-01-28 12:39:49 -0500

    get first product test

--- webscrape.py
+++ webscrape.py
@@ -4,10 +4,9 @@ from bs4 import BeautifulSoup
 url = "http://www.comparegroceryprices.org/search/data/comparison.shtml"
 
 response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+beautifulsoup = BeautifulSoup(response.text, "html.parser")
 
-products = soup.find_all("td", {"class": "list"})
-for product in products:
-    name = product.find("a").text
-    walmart_price = product.find("td", {"class": "walmart"}).text
-    print(name, walmart_price)
+results=beautifulsoup.find("tbody")
+food=results.find("td", width_="191")
+print(food)
+print(food.text)
