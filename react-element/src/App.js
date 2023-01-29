2dca830230acf947373698fbeaa561cfdaf4aad4
arghom <argho9703@gmail.com>
2023-01-28 21:04:37 -0500

    to main

--- react-element/src/App.js
+++ react-element/src/App.js
@@ -4,23 +4,32 @@ import './App.css';
 import React, { Component, createElement } from "react";
 import { findByLabelText } from '@testing-library/react';
 
-export default class TodoList extends Component {
+export default class groceryArrList extends Component {
   state = {
-    todo: [],
+    groceryArr: [],
     inputValue: ""
   };
+  price = {
+    //pull price from json file
+    priceArr: [],
+    priceVal: ""
+  };
+  investment = {
+    investArr: [],
+    investVal: ""
+  };
 
-  addTodo = () => {
-    this.setState({ todo: [this.state.inputValue, ...this.state.todo] });
-
-    // I want to insert separate paragraph tags (todos from this.state.todo) into the list element here
+  addGrocery = () => {
+    this.setState({ groceryArr: [this.state.inputValue, ...this.state.groceryArr] });
+    
+    // I want to insert separate paragraph tags (groceryArrs from this.state.groceryArr) into the list element here
   };
 
   handleKeyDown = (e) => {
     if (e.keyCode === 13) {
       if (this.state.inputValue === "") return;
 
-      this.addTodo();
+      this.addGrocery();
       this.state.inputValue = "";
     }
   };
@@ -63,21 +72,21 @@ export default class TodoList extends Component {
         <div style={mystyle}>
           <input
             type="text"
-            className="insertTodo"
+            className="insertgroceryArr"
             placeholder="Add a new item!"
             onChange={this.handleChange}
             onKeyDown={this.handleKeyDown}
             value={this.state.inputValue}
           />
-          <button onClick={this.addTodo}>Add to list</button>
+          <button onClick={this.addgroceryArr}>Add to list</button>
         </div>
         <div style={NPI}>
           <div className="list" style={listCategory}>
           <h3>
             ITEM
           </h3>
-            {this.state.todo.map((todo) => {
-              return <p>{todo}</p>;
+            {this.state.groceryArr.map((groceryArr) => {
+              return <p>{groceryArr}</p>;
             })}
           </div>
           <div style={listCategory}>
@@ -91,7 +100,7 @@ export default class TodoList extends Component {
             </h3>
           </div>
         </div>
-        
+
       </div>
     );
   }
